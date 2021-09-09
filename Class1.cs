83ed670150ef5cfc1817d7f39be78e62f1a0f1aa
a127807 <pete_fittante@progressive.com>
2021-09-09 15:40:29 -0400

    Added Data Store Project Configured Entity Framework Configured SiteContext

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MilSiteDataStore.EF
-{
-	public class Class1
-	{
-	}
-}
---
+++ SiteContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MilSiteCore.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MilSiteDataStore.EF
+{
+	public class SiteContext : DbContext
+	{
+		public SiteContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Location> Locations { get; set; }
+		public DbSet<LocationType> LocationTypes { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Location>()
+				.HasMany(l => l.LocationsTypes)
+				.WithOne(lt => lt.Location)
+				.HasForeignKey(lt => lt.LocId);
+
+			//seeding
+			modelBuilder.Entity<Location>().HasData(
+				new Location { LocId = 1, LocName = "Old River", MaxStay = 365, NumSpaces = 40, DateCreated = DateTime.ParseExact("2021-01-01", "yyyy-MM-dd", null), DateUpdated = DateTime.ParseExact("2021-01-02", "yyyy-MM-dd", null)}, 
+				new Location { LocId = 2, LocName = "Invansic", MaxStay = 36, NumSpaces = 4, DateCreated = DateTime.ParseExact("2021-01-01", "yyyy-MM-dd", null), DateUpdated = DateTime.ParseExact("2021-01-02", "yyyy-MM-dd", null) },
+				new Location { LocId = 2, LocName = "Neverland", MaxStay = 20, NumSpaces = 8, DateCreated = DateTime.ParseExact("2021-01-01", "yyyy-MM-dd", null), DateUpdated = DateTime.ParseExact("2021-01-02", "yyyy-MM-dd", null) }
+				);
+			modelBuilder.Entity<LocationType>().HasData(
+				new LocationType { LocTypeId = 1, LocId = 1, LocType = "Marina"  },
+				new LocationType { LocTypeId = 2, LocId = 2, LocType = "Campground" },
+				new LocationType { LocTypeId = 3, LocId = 3, LocType = "Hotel" }
+				);
+		}
+	}
+}
