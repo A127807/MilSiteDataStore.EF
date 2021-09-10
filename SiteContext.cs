e3a4d30aaf6347d45eb1c930b5163cb967f4dce6
a127807 <pete_fittante@progressive.com>
2021-09-10 17:29:10 -0400

    Install EF Created In-Memory database

--- SiteContext.cs
+++ SiteContext.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Json.Net;
+using Microsoft.EntityFrameworkCore;
 using MilSiteCore.Models;
 using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace MilSiteDataStore.EF
 {
@@ -17,21 +19,23 @@ namespace MilSiteDataStore.EF
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<Location>()
-				.HasMany(l => l.LocationsTypes)
-				.WithOne(lt => lt.Location)
-				.HasForeignKey(lt => lt.LocId);
+				.HasKey(l => l.LocId);
+			modelBuilder.Entity<LocationType>()
+				.HasKey(l => l.LocTypeId);
+
 
 			//seeding
 			modelBuilder.Entity<Location>().HasData(
-				new Location { LocId = 1, LocName = "Old River", MaxStay = 365, NumSpaces = 40, DateCreated = DateTime.ParseExact("2021-01-01", "yyyy-MM-dd", null), DateUpdated = DateTime.ParseExact("2021-01-02", "yyyy-MM-dd", null)}, 
+				new Location { LocId = 1, LocName = "Old River", MaxStay = 365, NumSpaces = 40, DateCreated = DateTime.ParseExact("2021-01-01", "yyyy-MM-dd", null), DateUpdated = DateTime.ParseExact("2021-01-02", "yyyy-MM-dd", null) },
 				new Location { LocId = 2, LocName = "Invansic", MaxStay = 36, NumSpaces = 4, DateCreated = DateTime.ParseExact("2021-01-01", "yyyy-MM-dd", null), DateUpdated = DateTime.ParseExact("2021-01-02", "yyyy-MM-dd", null) },
-				new Location { LocId = 2, LocName = "Neverland", MaxStay = 20, NumSpaces = 8, DateCreated = DateTime.ParseExact("2021-01-01", "yyyy-MM-dd", null), DateUpdated = DateTime.ParseExact("2021-01-02", "yyyy-MM-dd", null) }
+				new Location { LocId = 3, LocName = "Neverland", MaxStay = 20, NumSpaces = 8, DateCreated = DateTime.ParseExact("2021-01-01", "yyyy-MM-dd", null), DateUpdated = DateTime.ParseExact("2021-01-02", "yyyy-MM-dd", null) }
 				);
-			modelBuilder.Entity<LocationType>().HasData(
-				new LocationType { LocTypeId = 1, LocId = 1, LocType = "Marina"  },
-				new LocationType { LocTypeId = 2, LocId = 2, LocType = "Campground" },
-				new LocationType { LocTypeId = 3, LocId = 3, LocType = "Hotel" }
+			modelBuilder.Entity<LocationType>()
+				.HasData(
+				new LocationType { LocTypeId = 1, LocType = "Marina"},
+				new LocationType { LocTypeId = 2, LocType = "Campground" },
+				new LocationType { LocTypeId = 3, LocType = "Hotel" }
 				);
-		}
+	}
 	}
 }
